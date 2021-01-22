686bb0fef91d2efa299fcd3a81d893f7e92b50af
Jeremy Roussel <jeremy.roussel@umbrage.com>
2021-01-22 13:35:08 -0600

    Resolver for Slot + User 2

--- src/graphql/resolvers/slot.ts
+++ src/graphql/resolvers/slot.ts
@@ -2,6 +2,7 @@ import { Query, Resolver, Arg, Mutation } from "type-graphql";
 import Slot, { SlotInput } from "../schemas/slot";
 import SlotModel from "../../db/models/slot";
 import { v4 as uuidv4 } from "uuid";
+import { Model } from "sequelize";
 
 @Resolver((of) => Slot)
 export default class {
@@ -33,14 +34,38 @@ export default class {
     
     // Find All
     @Query(() => [Slot])
-    getSlots() {
+    allSlots() {
         return SlotModel.findAll();
     }
 
+    // Find by PK - takes ID, returns slot
     @Query(() => Slot)
-    getSlot(@Arg("id") id: string) {
+    checkSlot(@Arg("id") id: string) {
         return SlotModel.findByPk(id);
     }
+
+    // Select by Location
+    @Query(() =>  Slot)
+    slotsForLocation(@Arg("location") location: string) {
+        return SlotModel.findAll({ where: {location} })
+    }
+
+    // unReserveSlot() - takes slotID, returns t if successfully changes t -> f, f otherwise
+    @Query(() => Boolean)
+    async unReserveSlot(@Arg("id") id: string) {
+        const slot = await SlotModel.findByPk(id)
+        // check if reserved?
+
+        if (slot.isReserved) {
+            await slot.update({...slot, isReserved: false})
+            return true
+        }
+
+        return false
+
+    }
+
+    // slotsToReserve() - 
 
 
 
